42c95f2019d1e3ffe7a0febe3939d3db928d248d
WalterCorsini <corsini.walter@gmail.com>
2024-06-10 15:46:48 +0200

    add star

--- resources/views/partials/card.blade.php
+++ resources/views/partials/card.blade.php
@@ -2,6 +2,6 @@
     <img class="card-img-top" src="{{ $movie['image']}}" alt="Card image cap">
     <div class="card-body">
       <p class="card-text">{{$movie['original_title']}}</p>
-      <p class="card-text">{{$movie['vote']}} / 10</p>
+      <p class="card-text"> {{!!$movie['star']!!}}</p>
     </div>
   </div>
