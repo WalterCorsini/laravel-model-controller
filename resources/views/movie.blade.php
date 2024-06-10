05cda9e4b2a2cdbea1272849b26f646ff8fa7634
WalterCorsini <corsini.walter@gmail.com>
2024-06-10 15:27:58 +0200

    add partials card

--- resources/views/movie.blade.php
+++ resources/views/movie.blade.php
@@ -4,12 +4,6 @@
 @section('content')
     <h1 class="fw-bold pt-5">Movie</h1>
     @foreach ($moviesList as $movie)
-    <div class="card" style="width: 18rem;">
-        <img class="card-img-top" src="{{ $movie['image']}}" alt="Card image cap">
-        <div class="card-body">
-          <p class="card-text">{{$movie['original_title']}}</p>
-          <p class="card-text">{{$movie['vote']}} / 10</p>
-        </div>
-      </div>
+        @include('partials.card') 
     @endforeach
 @endsection
