119bedc7c88564de2f7cc918e4832182cc0cc7de
WalterCorsini <corsini.walter@gmail.com>
2024-06-10 17:08:30 +0200

    fix condition star

--- resources/views/partials/card.blade.php
+++ resources/views/partials/card.blade.php
@@ -3,11 +3,12 @@
     <div class="card-body">
       <p class="card-text">{{$movie['original_title']}}</p>
       
-        @for($i=0; $i<floor($movie['vote']/2); $i++)
-            <i class='fa-solid fa-star text-gold'></i>
-        @endfor
-        @for($i=0; $i<5-floor($movie['vote']/2); $i++)
-            <i class='fa-regular fa-star text-gold'></i>
+        @for($i=0; $i<5; $i++)
+            @if(floor($movie['vote']/2)>$i)
+              <i class='fa-solid fa-star text-gold'></i>                
+            @else
+              <i class='fa-regular fa-star text-gold'></i>
+            @endif
         @endfor
     </div>
   </div>
