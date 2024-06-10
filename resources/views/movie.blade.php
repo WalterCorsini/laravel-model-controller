45ed9c0fe3dd5d7291a0775c4985f787c26e4cc2
WalterCorsini <corsini.walter@gmail.com>
2024-06-10 15:30:28 +0200

    add card style

--- resources/views/movie.blade.php
+++ resources/views/movie.blade.php
@@ -2,8 +2,10 @@
 
 {{--  qui il contenuto della pagine home page --}}
 @section('content')
-    <h1 class="fw-bold pt-5">Movie</h1>
+    <h1 class="fw-bold pt-5 pb.3 text-center">Movie</h1>
+    <div class="d-flex w-75 m-auto flex-wrap gap-4">
     @foreach ($moviesList as $movie)
         @include('partials.card') 
     @endforeach
+    </div>
 @endsection
