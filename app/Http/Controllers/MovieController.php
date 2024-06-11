1b1c780cea93560e1d33314d1ce1831ffb12d359
WalterCorsini <corsini.walter@gmail.com>
2024-06-11 10:29:16 +0200

    fix data with -> and not ['']

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Models\Movie;
 
 use Illuminate\Http\Request;
-
 class MovieController extends Controller
 {
     public function index(){
--- resources/views/partials/card.blade.php
+++ resources/views/partials/card.blade.php
@@ -1,10 +1,10 @@
 <div class="card" style="width: 18rem;">
-    <img class="card-img-top" src="{{ $movie['image']}}" alt="Card image cap">
+    <img class="card-img-top" src="{{ $movie->image}}" alt="Card image cap">
     <div class="card-body">
-      <p class="card-text">{{$movie['original_title']}}</p>
+      <p class="card-text">{{$movie->original_title}}</p>
       
         @for($i=0; $i<5; $i++)
-            @if(floor($movie['vote']/2)>$i)
+            @if(floor($movie->vote/2)>$i)
               <i class='fa-solid fa-star text-gold'></i>                
             @else
               <i class='fa-regular fa-star text-gold'></i>
